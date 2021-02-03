6bef62d68f3cd41029e1a4f3df900299f970d049
miserii <miseri.osk12@gmail.com>
2021-02-03 14:48:04 +0900

    add gitignore

--- Pods/Target Support Files/Pods-GitHub-Search-GitHub-SearchUITests/Pods-GitHub-Search-GitHub-SearchUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GitHub_Search_GitHub_SearchUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GitHub_Search_GitHub_SearchUITests
-@end
