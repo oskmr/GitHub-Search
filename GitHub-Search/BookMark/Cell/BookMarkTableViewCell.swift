acee7287abc51692d8b243701a8776c999da10a7
miseri <miseri.osk12@gmail.com>
2021-02-24 01:04:00 +0900

    リポジトリのurlとnameを読み取り

--- GitHub-Search/BookMark/Cell/BookMarkTableViewCell.swift
+++ GitHub-Search/BookMark/Cell/BookMarkTableViewCell.swift
@@ -12,4 +12,16 @@ class BookMarkTableViewCell: UITableViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var url: UILabel!
 
+    static func bookMarkData(forKey key: String) -> String {
+        let data = UserDefaults.standard.object(forKey: key) as? Data
+        let unarchiveObject = NSKeyedUnarchiver.unarchiveObject(with: data!)
+
+        return ""
+    }
+
+    func prepareUI() {
+        title.text = BookMarkTableViewCell.bookMarkData(forKey: "key")
+        url.text = BookMarkTableViewCell.bookMarkData(forKey: "key")
+    }
+
 }
