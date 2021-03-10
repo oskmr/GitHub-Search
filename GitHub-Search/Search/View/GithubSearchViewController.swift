e573145ab59059c705ad9f1b33e106402a88b1c9
miseri <miseri.osk12@gmail.com>
2021-03-11 00:21:40 +0900

    filter修正

--- GitHub-Search/Search/View/GithubSearchViewController.swift
+++ GitHub-Search/Search/View/GithubSearchViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxOptional
 
 final class GithubSearchViewController: UIViewController {
 
@@ -45,10 +46,10 @@ final class GithubSearchViewController: UIViewController {
 
     private func bindInputStream() {
         // 0.2以上,変化している,nilじゃない,文字数0以上だったらテキストを流す
-        let searchTextObservable = searchTextField.rx.text.orEmpty
+        let searchTextObservable = searchTextField.rx.text
             .debounce(RxTimeInterval.milliseconds(200), scheduler: MainScheduler.instance)
-            .distinctUntilChanged().filter { $0.isEmpty }
-        // ここのfilterNil消す書き方にできる？
+            .distinctUntilChanged().filterNil().filter { $0.isNotEmpty }
+        // ここのfilterNil消したらインクリメンタルサーチが死ぬ
 
         let sortTypeObservable = Observable.merge(
             Observable.just(sortTypeSegmentedControl.selectedSegmentIndex),
