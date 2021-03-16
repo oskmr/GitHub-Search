688dd67014878db82fd5dc0f58c81fb0d79e30f3
miseri <miseri.osk12@gmail.com>
2021-03-16 22:05:29 +0900

    add listTest

--- GitHub-SearchTests/GithubListViewModelTest.swift
+++ GitHub-SearchTests/GithubListViewModelTest.swift
@@ -5,14 +5,39 @@
 //  Created by OsakaMiseri on 2021/03/14.
 //
 
-@testable import GitHub_Search
+import RxSwift
+import RxCocoa
 import XCTest
+@testable import GitHub_Search
 
 class GithubListViewModelTest: XCTestCase {
 
     func test() {
         let dependency = Dependency()
         let viewModel = dependency.testTarget
+
+        let repository = GithubListEntity(id: 1,
+                                            name: "testName",
+                                            fullName: "testFullName",
+                                            htmlURL: "testHtmlUrl")
+        let disposeBag = DisposeBag()
+
+        // let searchResults =
+        // let searchTextResult =
+
+        let searchText = "test"
+        guard let searchTextResultValue = searchTextResult.value else {
+            XCTFail(" searchTextResult.value is nil")
+            return
+        }
+        XCTAssertEqual(searchTextResultValue, searchText)
+
+        guard let repositoryResult = searchResults.value.first else {
+            XCTFail("searchResults.value.first is nil")
+            return
+        }
+        XCTAssertEqual(repositoryResult.name, repository.name)
+        XCTAssertEqual(repositoryResult.htmlURL, repository.htmlURL)
     }
 
 }
