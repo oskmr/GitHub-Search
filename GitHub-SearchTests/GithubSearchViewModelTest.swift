06fde39d0513a36f41c527b2b3a4cbbf6405869b
miseri <miseri.osk12@gmail.com>
2021-03-16 23:59:41 +0900

    add  searchTest

--- GitHub-SearchTests/GithubSearchViewModelTest.swift
+++ GitHub-SearchTests/GithubSearchViewModelTest.swift
@@ -5,14 +5,46 @@
 //  Created by OsakaMiseri on 2021/03/14.
 //
 
-@testable import GitHub_Search
+import RxSwift
+import RxCocoa
 import XCTest
+@testable import GitHub_Search
 
 class GithubSearchViewModelTest: XCTestCase {
 
     func test() {
         let dependency = Dependency()
         let viewModel = dependency.testTarget
+
+        let repository = GithubEntity(id: 1, name: "", fullName: "")
+        let disposeBag = DisposeBag()
+
+        let searchResults = BehaviorRelay<[GithubEntity]>(value: [])
+        dependency.testTarget
+            .bind(to: searchResults)
+            .disposed(by: disposeBag)
+
+        let searchTextResult = BehaviorRelay<String?>(value: nil)
+        dependency.testTarget
+            .bind(to: searchTextResult)
+            .disposed(by: disposeBag)
+
+        let searchText = "test"
+
+        guard let searchTextResultValue = searchTextResult.value else {
+            XCTFail(" searchTextResult.value is nil")
+            return
+        }
+
+        XCTAssertEqual(searchTextResultValue, searchText)
+
+        guard let repositoryResult = searchResults.value.first else {
+            XCTFail("searchResults.value.first is nil")
+            return
+        }
+
+        XCTAssertEqual(repositoryResult.fullName, repository.name)
+        XCTAssertEqual(repositoryResult.urlStr, repository.urlStr)
     }
 
 }
@@ -20,9 +52,11 @@ class GithubSearchViewModelTest: XCTestCase {
 private extension GithubSearchViewModelTest {
 
     struct Dependency {
+        let searchText = PublishRelay<String>()
         let testTarget: GithubSearchViewModel
+
         init() {
-            self.testTarget = .init()
+            self.testTarget = GithubSearchViewModel()
         }
     }
 
